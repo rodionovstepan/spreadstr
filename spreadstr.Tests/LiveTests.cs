7d76bd97aab7947f1cc71eb23d26fe4f271def74
rodionovstepan <rodionov.stepan@byndyusoft.com>
2014-08-20 14:59:58 +0600

    spreadstr simple imlementation with live test

---
+++ spreadstr.Tests/LiveTests.cs
@@ -0,0 +1,28 @@
+﻿namespace spreadstr.Tests
+{
+    using System.IO;
+    using Impl;
+    using NUnit.Framework;
+
+    public class LiveTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var spreadstr = new Spreadstr();
+            var converter = new TestSpreadstrConverter();
+            var items = new[]
+                        {
+                            new TestItem("Ivan", "Ivanov"),
+                            new TestItem("Semen", "Semenov"),
+                            new TestItem("Sergey", "Sergeev"),
+                            new TestItem("Viktor", "Viktorov"),
+                            new TestItem("Vladimir", "Vladimirov")
+                        };
+
+            var spreadsheet = spreadstr.Generate(items, converter);
+
+            File.WriteAllBytes("result.xlsx", spreadsheet.Content);
+        }
+    }
+}
---
+++ spreadtr/Impl/Spreadstr.cs
@@ -0,0 +1,41 @@
+﻿namespace spreadstr.Impl
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using OfficeOpenXml;
+
+    public class Spreadstr : ISpreadstr
+    {
+        public Spreadsheet Generate<T>(IEnumerable<T> items, ISpreadstrConverter<T> converter)
+        {
+            var sheetname = string.Format("Spreadsheet at {0:dd-MM-yyyy}", DateTime.Now);
+
+            using (var excel = new ExcelPackage())
+            {
+                var worksheet = excel.Workbook.Worksheets.Add(sheetname);
+                var rowIndex = 1;
+
+                foreach (var item in items)
+                {
+                    var row = converter.Convert(item);
+                    var columnIndex = 1;
+
+                    foreach (var column in row.Columns)
+                    {
+                        worksheet.Cells[rowIndex, columnIndex++].Value = column;
+                        worksheet.Column(columnIndex).AutoFit();
+                    }
+
+                    ++rowIndex;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    excel.SaveAs(stream);
+                    return new Spreadsheet(stream.ToArray());
+                }
+            }
+        }
+    }
+}
